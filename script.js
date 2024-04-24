ade6da8994b7b5915e1ab5064c6a3cb147fcec54
Jennaicode <165965688+Jennaicode@users.noreply.github.com>
2024-04-24 00:06:51 +0000

    Just trying to commit

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ function toggleMenu() {
     }
 }
 
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
     });
 });
